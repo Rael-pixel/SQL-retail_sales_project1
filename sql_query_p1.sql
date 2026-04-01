5ec0bca3dd24067f9892a52b69fc337ec559ae4b
Rael_DS <ruwarael@gmail.com>
2026-04-01 17:41:25 +0300

    Restore SQL queries and table creation for retail sales

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -85,8 +85,6 @@ SELECT DISTINCT category
 FROM retail_sales;
 
 
---Business key problems and answers
--- My Analysis & Findings
 -- Q.1 Retrieve all columns for sales made on '2022-11-05
 -- Q.2 Retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
 -- Q.3 Calculate the total sales (total_sale) for each category.
