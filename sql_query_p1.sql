0da9ac7f1052e846baa4f9cd8e1eafd37d7f3145
Rael_DS <ruwarael@gmail.com>
2026-04-01 17:39:31 +0300

    Restore SQL queries and table creation for retail sales

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -87,16 +87,19 @@ FROM retail_sales;
 
 --Business key problems and answers
 -- My Analysis & Findings
--- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
--- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
--- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
--- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
--- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
--- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
--- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
--- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales in the clothing category
--- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
--- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
+-- Q.1 Retrieve all columns for sales made on '2022-11-05
+-- Q.2 Retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
+-- Q.3 Calculate the total sales (total_sale) for each category.
+-- Q.4 Find the average age of customers who purchased items from the 'Beauty' category.
+-- Q.5 Find all transactions where the total_sale is greater than 1000.
+-- Q.6 Find the total number of transactions (transaction_id) made by each gender in each category.
+-- Q.7 Calculate the average sale for each month. Find out best selling month in each year
+-- Q.8 Find the top 5 customers based on the highest total sales in the clothing category
+-- Q.9 Find the number of unique customers who purchased items from each category.
+-- Q.10 Write a query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
+--Q.11: Which customers contribute the most revenue?
+--Q.12: Which age group generate the highest revenue?
+--Q13: How does revenue change month to month?
 
 
 -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05'
@@ -185,5 +188,37 @@ SELECT
 	COUNT(*) AS total_orders
 FROM sales_per_shift
 GROUP BY shift
+
+--Q.11: Which customers contribute the most revenue?
+
+SELECT 
+    customer_id,
+    SUM(total_sale) AS total_revenue
+FROM retail_sales
+GROUP BY customer_id
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+--Q.12: Which age group generate the highest revenue?
+SELECT 
+    CASE 
+        WHEN age BETWEEN 18 AND 25 THEN '18-25'
+        WHEN age BETWEEN 26 AND 35 THEN '26-35'
+        WHEN age BETWEEN 36 AND 50 THEN '36-50'
+        ELSE '50+'
+    END AS age_group,
+    SUM(total_sale) AS total_revenue
+FROM retail_sales
+GROUP BY age_group
+ORDER BY total_revenue DESC;
+
+--Q13: How does revenue change month to month?
+SELECT 
+    EXTRACT(YEAR FROM sale_date) AS year,
+    EXTRACT(MONTH FROM sale_date) AS month,
+    SUM(total_sale) AS monthly_revenue
+FROM retail_sales
+GROUP BY year, month
+ORDER BY year, month;
 
 --End of project
